eedf58d562b6da7b2db8eb39ec5689609cd0f0f8
Kulpot <kulpotwahu@gmail.com>
2024-06-15 11:07:10 +0800

    END

--- IEEE Floating Point Standard (The Implicit 1)/Mantissa.cpp
+++ IEEE Floating Point Standard (The Implicit 1)/Mantissa.cpp
@@ -4,5 +4,5 @@
 
 //IEEE Floating Point Standard (The Implicit 1)  //ref link:https://www.youtube.com/watch?v=wu8nWzNaVbA&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=4
 
-// 11011
+// 11011 
 // 1.11011		implicit 1. in mantissa
